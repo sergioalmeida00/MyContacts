57db8e5c8ecf63441b96cb4e8c6b845667fffa73
sergio almeida <sergioalmeidaa00@gmail.com>
2023-02-28 11:36:44 -0300

    REFECTOR: reconfigurado o eslint

--- src/controllers/ContactController.ts
+++ src/controllers/ContactController.ts
@@ -1,85 +1,87 @@
-import { Request, Response } from "express";
-import { z } from "zod";
-import ContactRepository from "../repositories/ContactRepository";
+import { Request, Response } from 'express'
+import { z } from 'zod'
+import ContactRepository from '../repositories/ContactRepository'
 
-class ContactController{
-  //Listar todos os registros
-  async index(request:Request, response:Response):Promise<Response>{
-    const contacts = await ContactRepository.findAll();
+class ContactController {
+  // Listar todos os registros
+  async index(request: Request, response: Response): Promise<Response> {
+    const contacts = await ContactRepository.findAll()
 
-    return response.status(200).json({contacts});
+    return response.status(200).json({ contacts })
   }
 
-  //Obter um registro
-  async show(request:Request, response:Response):Promise<Response>{
-     const getContactsParamSchema = z.object({
-      id: z.string().uuid()
-     })
+  // Obter um registro
+  async show(request: Request, response: Response): Promise<Response> {
+    const getContactsParamSchema = z.object({
+      id: z.string().uuid({ message: 'Não é um ID valido' }),
+    })
+    const { id } = getContactsParamSchema.parse(request.params)
 
-     const {id} = getContactsParamSchema.parse(request.params);
+    if (!id) {
+      return response.status(404).json({ message: 'Id invalid.' })
+    }
+    const contactResult = await ContactRepository.findById({ id })
 
-     if(!id){
-        return response.status(404).json({message:"Id invalid."})
-     }
-     const contactResult = await ContactRepository.findById({id});
-
-     return response.status(200).json({contactResult});
-
+    return response.status(200).json({ contactResult })
   }
 
-  //Criar um novo registro
-  async store(request:Request, response:Response){
-    const {name, email, phone, category_id} = request.body;
+  // Criar um novo registro
+  async store(request: Request, response: Response) {
+    const { name, email, phone, category_id } = request.body
 
-    const contactEmailExists = await ContactRepository.findByEmail({email});
+    const contactEmailExists = await ContactRepository.findByEmail({ email })
 
-    if(contactEmailExists){
-      return response.status(404).json({error:'This e-mail is already in use!'});
+    if (contactEmailExists) {
+      return response
+        .status(404)
+        .json({ error: 'This e-mail is already in use!' })
     }
 
-    await ContactRepository.create({name, email, phone, category_id});
+    await ContactRepository.create({ name, email, phone, category_id })
 
-    return response.sendStatus(201);
+    return response.sendStatus(201)
   }
 
-  //Editar um registro
-  async update(request:Request, response:Response){
-    const { name, email, phone, category_id } = request.body;
-    const {id} = request.params;
+  // Editar um registro
+  async update(request: Request, response: Response) {
+    const { name, email, phone, category_id } = request.body
+    const { id } = request.params
 
-    const contactExists = await ContactRepository.findById({id});
-    const contactEmailExists = await ContactRepository.findByEmail({email});
+    const contactExists = await ContactRepository.findById({ id })
+    const contactEmailExists = await ContactRepository.findByEmail({ email })
 
-    if(!contactExists){
-      return response.status(404).json({error:"Contact does not exist"});
+    if (!contactExists) {
+      return response.status(404).json({ error: 'Contact does not exist' })
     }
 
-    if(contactEmailExists && contactEmailExists.id !== id){
-      return response.status(400).json({error:'This e-mail is already in use!'});
+    if (contactEmailExists && contactEmailExists.id !== id) {
+      return response
+        .status(400)
+        .json({ error: 'This e-mail is already in use!' })
     }
 
-    await ContactRepository.update({name, email, phone, category_id,id});
+    await ContactRepository.update({ name, email, phone, category_id, id })
 
-    return response.sendStatus(204);
+    return response.sendStatus(204)
   }
 
-  //Deletar um registro
-  async delete(request:Request, response:Response){
+  // Deletar um registro
+  async delete(request: Request, response: Response) {
     const getContactIdParamSchema = z.object({
-      id:z.string().uuid()
-    });
+      id: z.string().uuid(),
+    })
 
-    const {id} = getContactIdParamSchema.parse(request.params);
+    const { id } = getContactIdParamSchema.parse(request.params)
 
-    const contactExists = await ContactRepository.findById({id});
+    const contactExists = await ContactRepository.findById({ id })
 
-    if(!contactExists){
-      return response.status(404).json({error:"Contact does not exist"})
+    if (!contactExists) {
+      return response.status(404).json({ error: 'Contact does not exist' })
     }
 
-    await ContactRepository.delete({id});
+    await ContactRepository.delete({ id })
 
-    return response.sendStatus(204);
+    return response.sendStatus(204)
   }
 }
 
--- src/repositories/ContactRepository.ts
+++ src/repositories/ContactRepository.ts
@@ -1,49 +1,71 @@
-import { knex } from "../database";
-import {randomUUID } from 'node:crypto'
-import { InputContactDto, InputContactFindByIdDto, InputContactFindEmailDto, OutputContactDto } from "../dto/contact.dto";
+import { knex } from '../database'
+import { randomUUID } from 'node:crypto'
+import {
+  InputContactDto,
+  InputContactFindByIdDto,
+  InputContactFindEmailDto,
+  OutputContactDto,
+} from '../dto/contact.dto'
 
-
-class ContactRepository{
-  async findAll():Promise<InputContactDto[]>{
-    const contacts = await knex<InputContactDto>('contacts').select('*');
+class ContactRepository {
+  async findAll(): Promise<InputContactDto[]> {
+    const contacts = await knex<InputContactDto>('contacts').select('*')
     return contacts
   }
 
-  async findById({id}:InputContactFindByIdDto):Promise<OutputContactDto | undefined>{
-    const contactById = await knex<OutputContactDto>('contacts').select('*').where('id',id).first();
-    return contactById;
+  async findById({
+    id,
+  }: InputContactFindByIdDto): Promise<OutputContactDto | undefined> {
+    const contactById = await knex<OutputContactDto>('contacts')
+      .select('*')
+      .where('id', id)
+      .first()
+    return contactById
   }
 
-  async findByEmail({email}:InputContactFindEmailDto):Promise<OutputContactDto | undefined>{
+  async findByEmail({
+    email,
+  }: InputContactFindEmailDto): Promise<OutputContactDto | undefined> {
+    const contactByEmail = await knex<OutputContactDto>('contacts')
+      .where({ email })
+      .select('*')
+      .first()
 
-    const contactByEmail = await knex<OutputContactDto>('contacts').where({email}).select('*').first();
-
-    return contactByEmail;
+    return contactByEmail
   }
 
-  async create({name, email, phone, category_id}:InputContactDto):Promise<void>{
+  async create({
+    name,
+    email,
+    phone,
+    category_id,
+  }: InputContactDto): Promise<void> {
     await knex<InputContactDto>('contacts').insert({
-      id:randomUUID(),
+      id: randomUUID(),
       name,
       email,
       phone,
-      category_id
+      category_id,
     })
   }
 
-  async delete({id}:InputContactFindByIdDto):Promise<void>{
-    await knex<InputContactFindByIdDto>('contacts').where('id',id).del();
+  async delete({ id }: InputContactFindByIdDto): Promise<void> {
+    await knex<InputContactFindByIdDto>('contacts').where('id', id).del()
   }
 
-  async update({name,email,phone,category_id, id}:InputContactDto):Promise<void>{
-
-   await knex<InputContactDto>('contacts').where('id',id).update({
+  async update({
+    name,
+    email,
+    phone,
+    category_id,
+    id,
+  }: InputContactDto): Promise<void> {
+    await knex<InputContactDto>('contacts').where('id', id).update({
       name,
       email,
       phone,
-      category_id
+      category_id,
     })
-
   }
 }
-export = new ContactRepository();
+export = new ContactRepository()
